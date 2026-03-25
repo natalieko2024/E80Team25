0c864e27864a9ec085e40e0297a5b8ead2c3d7ef
VxSv <137260807+VxSv@users.noreply.github.com>
2026-03-24 20:43:56 -0700

    Update SurfaceControl.h

--- libraries/main/SurfaceControl.h
+++ libraries/main/SurfaceControl.h
@@ -35,11 +35,11 @@ public:
   float dist;            // distance to waypoint
   float u;               // control effort
   float Kp=10.0;         // proportional control gain
-  float Kr=1.0;          // right motor gain correction
-  float Kl=1.0;          // left motor gain correction
-  float avgPower = 10.0; // average forward thrust, changed to 50 from 10 per 4.5 instructions
-  float uR = uR // *Kr;              // right motor effort
-  float uL = uL // *Kl;              // left motor effort
+  float Kr=1.0;          // right motor gain correction; change if motors are not running at same speed
+  float Kl=1.0;          // left motor gain correction; change if motors are not running at same speed
+  float avgPower = 10.0; // average forward thrust, change to 50 from 10 per 4.5 instructions
+  float uR = uR; // *Kr;              // right motor effort
+  float uL = uL; // *Kl;              // left motor effort
 
   // if (uR > 127) uR = 127; //uR control values
   // if (uR < 0) uR = 0;
