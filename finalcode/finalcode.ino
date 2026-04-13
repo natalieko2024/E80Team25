42f81a7d2ce0b066e319e8cd22148674c9770c61
natalieko2024 <ko12nat@gmail.com>
2026-04-12 17:38:33 -0700

    Compiled Dive/Surface/MMT

--- finalcode/finalcode.ino
+++ finalcode/finalcode.ino
@@ -1,6 +1,6 @@
 /********
-Base Code: E80 Lab 7 Surface Activity Code
-Original Authors:
+Based on E80 Lab 7 Surface and Dive Activity Codes
+Authors:
     Omar Aleman (oaleman@g.hmc.edu) '21 (contributed 2019)
     Wilson Ives (wives@g.hmc.edu) '20 (contributed in 2018)
     Christopher McElroy (cmcelroy@g.hmc.edu) '19 (contributed in 2017)  
@@ -22,6 +22,7 @@ Written for Sp26 Team 25 by Natalie Ko (nko@g.hmc.edu)
 #include <SensorGPS.h>
 #include <SensorIMU.h>
 #include <XYStateEstimator.h>
+#include <ZStateEstimator.h>
 #include <ADCSampler.h>
 #include <ErrorFlagSampler.h>
 #include <ButtonSampler.h> // A template of a data source library
@@ -29,6 +30,7 @@ Written for Sp26 Team 25 by Natalie Ko (nko@g.hmc.edu)
 #include <Logger.h>
 #include <Printer.h>
 #include <SurfaceControl.h>
+#include <DepthControl.h>
 #define UartSerial Serial1
 #include <GPSLockLED.h>
 
@@ -39,6 +41,8 @@ Written for Sp26 Team 25 by Natalie Ko (nko@g.hmc.edu)
 MotorDriver motor_driver;
 XYStateEstimator xy_state_estimator;
 SurfaceControl surface_control;
+ZStateEstimator z_state_estimator;
+DepthControl depth_control;
 SensorGPS gps;
 Adafruit_GPS GPS(&UartSerial);
 ADCSampler adc;
@@ -49,12 +53,12 @@ Logger logger;
 Printer printer;
 GPSLockLED led;
 
-Magnetometer mmt;
-
 // loop start recorder
 int loopStartTime;
 int currentTime;
 volatile bool EF_States[NUM_FLAGS] = {1,1,1};
+
+Magnetometer mmt;
 
 // magnetometer constants
 uint8_t revid;
@@ -69,6 +73,8 @@ void setup() {
   logger.include(&gps);
   logger.include(&xy_state_estimator);
   logger.include(&surface_control);
+  logger.include(&z_state_estimator);
+  logger.include(&depth_control);
   logger.include(&motor_driver);
   logger.include(&adc);
   logger.include(&ef);
@@ -85,14 +91,21 @@ void setup() {
   motor_driver.init();
   led.init();
 
-
   int navigateDelay = 0; // how long robot will stay at surface waypoint before continuing (ms)
 
   const int num_surface_waypoints = 3; // Number of ordered pairs of surface waypoints. (e.g., if surface_waypoints is {x0,y0,x1,y1} then num_surface_waypoints is 2.) Set to 0 if only doing depth control 
+  // START, END, START
   double surface_waypoints [] = { 125, -40, 150, -40, 125, -40 };   // listed as x0,y0,x1,y1, ... etc.
   surface_control.init(num_surface_waypoints, surface_waypoints, navigateDelay);
-  
+
+  int diveDelay = 0; // how long robot will stay at depth waypoint before continuing (ms)
+
+  const int num_depth_waypoints = 10;
+  double depth_waypoints [] = { 0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5 };  // listed as z0,z1,... etc.
+  depth_control.init(num_depth_waypoints, depth_waypoints, diveDelay);
+
   xy_state_estimator.init(); 
+  z_state_estimator.init();
 
   mmt.init();
   pinMode(DRDYPin, INPUT_PULLUP); // We want to read
@@ -127,6 +140,8 @@ void setup() {
   button_sampler.lastExecutionTime     = loopStartTime - LOOP_PERIOD + BUTTON_LOOP_OFFSET;
   xy_state_estimator.lastExecutionTime = loopStartTime - LOOP_PERIOD + XY_STATE_ESTIMATOR_LOOP_OFFSET;
   surface_control.lastExecutionTime    = loopStartTime - LOOP_PERIOD + SURFACE_CONTROL_LOOP_OFFSET;
+  z_state_estimator.lastExecutionTime  = loopStartTime - LOOP_PERIOD + Z_STATE_ESTIMATOR_LOOP_OFFSET;
+  depth_control.lastExecutionTime      = loopStartTime - LOOP_PERIOD + DEPTH_CONTROL_LOOP_OFFSET;
   logger.lastExecutionTime             = loopStartTime - LOOP_PERIOD + LOGGER_LOOP_OFFSET;
   mmt.lastExecutionTime                = loopStartTime - LOOP_PERIOD + MMT_LOOP_OFFSET;
 }
@@ -149,10 +164,13 @@ void loop() {
     printer.printValue(4,xy_state_estimator.printState());  
     printer.printValue(5,surface_control.printWaypointUpdate());
     printer.printValue(6,surface_control.printString());
-    printer.printValue(7,motor_driver.printState());
-    printer.printValue(8,imu.printRollPitchHeading());        
-    printer.printValue(9,imu.printAccels());
-    printer.printValue(10, mmt.printValues());
+    printer.printValue(7,z_state_estimator.printState());  
+    printer.printValue(8,depth_control.printWaypointUpdate());
+    printer.printValue(9,depth_control.printString());
+    printer.printValue(10,motor_driver.printState());
+    printer.printValue(11,imu.printRollPitchHeading());        
+    printer.printValue(12,imu.printAccels());
+    printer.printValue(13,mmt.printValues());
     printer.printToSerial();  // To stop printing, just comment this line out
   }
 
@@ -170,6 +188,31 @@ void loop() {
         surface_control.atPoint = false;   // get ready to go to the next point
       }
       motor_driver.drive(surface_control.uL,surface_control.uR,0);
+    }
+  }
+
+  /* ROBOT CONTROL Finite State Machine */
+  if ( currentTime-depth_control.lastExecutionTime > LOOP_PERIOD ) {
+    depth_control.lastExecutionTime = currentTime;
+    if ( depth_control.diveState ) {      // DIVE STATE //
+      depth_control.complete = false;
+      if ( !depth_control.atDepth ) {
+        depth_control.dive(&z_state_estimator.state, currentTime);
+      }
+      else {
+        depth_control.diveState = false; 
+        depth_control.surfaceState = true;
+      }
+      motor_driver.drive(0,0,depth_control.uV);
+    }
+    if ( depth_control.surfaceState ) {     // SURFACE STATE //
+      if ( !depth_control.atSurface ) { 
+        depth_control.surface(&z_state_estimator.state);
+      }
+      else if ( depth_control.complete ) { 
+        delete[] depth_control.wayPoints;   // destroy depth waypoint array from the Heap
+      }
+      motor_driver.drive(0,0,depth_control.uV);
     }
   }
   
@@ -211,6 +254,11 @@ void loop() {
     xy_state_estimator.updateState(&imu.state, &gps.state);
   }
   
+  if ( currentTime-z_state_estimator.lastExecutionTime > LOOP_PERIOD ) {
+    z_state_estimator.lastExecutionTime = currentTime;
+    z_state_estimator.updateState(analogRead(PRESSURE_PIN));
+  }
+
   if ( currentTime-led.lastExecutionTime > LOOP_PERIOD ) {
     led.lastExecutionTime = currentTime;
     led.flashLED(&gps.state);
